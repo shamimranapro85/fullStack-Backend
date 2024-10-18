08ea58226b499003044517824ed4df3be2ca33ab
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-10-18 07:02:30 +0600

    add atlas mongodb problem solved

--- service/secreate.js
+++ service/secreate.js
@@ -6,7 +6,8 @@
  const jsonKey = process.env.jsonWEbToken
  const mainURL = process.env.mainUrl
  const mongoConnect_URI  = process.env.mongoConnect_URI
+ const Uri  = process.env.mongoConnect_URI
  
 
 
- module.exports = { port , jsonKey,mainURL,mongoConnect_URI}
+ module.exports = { port , jsonKey,mainURL, mongoConnect_URI}
