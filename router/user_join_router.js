d10a53466c158a0e8c3e87080b294c089013ca7e
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-10-28 09:00:18 +0600

    httpOnly use and sameSite Laz

--- router/user_join_router.js
+++ router/user_join_router.js
@@ -50,9 +50,10 @@ User_join_router.get("/logout", LogOut); // লগিন.................-------
 User_join_router.get("/cookie",async (req, res) => {
   await res.cookie("cookie", "cookieValue", {
     domain: "localhost",
-    sameSite: "None",
+    sameSite: "Lax",
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     credentials:true,
+    httpOnly: true
   });
   console.log("success");
   
